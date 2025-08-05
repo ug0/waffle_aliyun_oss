5c6200373082dd524dff6da8560ef2a620658833
ug0 <18638328+ug0@users.noreply.github.com>
2025-08-05 09:11:02 +0800

    Update aliyun_oss to v3.1.1 and aliyun_util to 0.4.2

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule WaffleAliyunOss.MixProject do
   defp deps do
     [
       {:waffle, "~> 1.1"},
-      {:aliyun_oss, "~> 3.1.0"},
+      {:aliyun_oss, "~> 3.1"},
       {:ex_doc, "~> 0.20", only: :dev}
     ]
   end
